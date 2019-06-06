eadd417e2b11dcf53ce13a26f642fddc30c2c6ca
Channet <channet@inteligenciatotal.com>
2019-06-06 18:43:37 -0500

    jueves6junio

--- Shop.Web/Data/Entities/Product.cs
+++ Shop.Web/Data/Entities/Product.cs
@@ -9,11 +9,11 @@
     public class Product
     {
         public int Id { get; set; }
-        public string Name { get; set; }
+        [MaxLength(50,ErrorMessage ="El campo {0} requiere ser máximo {1} de largo")] [Required] public string Name { get; set; }
         [DisplayFormat(DataFormatString = "{0:C2}", ApplyFormatInEditMode = false)] public decimal Price { get; set; }
         [Display(Name = "Image")] public string ImageUrl { get; set; }
-        [Display(Name = "Last Purchase")] public DateTime LastPurchase { get; set; }
-        [Display(Name = "Last Sale")] public DateTime LastSale { get; set; }
+        [Display(Name = "Last Purchase")] public DateTime? LastPurchase { get; set; }
+        [Display(Name = "Last Sale")] public DateTime? LastSale { get; set; }
         [Display(Name = "IsAvalable")] public bool IsAvalaible { get; set; }
         [Display(Name = "Stock")] [DisplayFormat(DataFormatString = "{0:N2}", ApplyFormatInEditMode = false)] public double Stock { get; set; }
     }
