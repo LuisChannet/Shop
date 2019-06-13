f41cec969dcc0ca39359c06e3a1fd1cb1af78b5f
Channet <channet@inteligenciatotal.com>
2019-06-13 12:53:44 -0500

    isRefreshing ListView

--- Shop.UIForms/Shop.UIForms/ViewModels/ProductsViewModel.cs
+++ Shop.UIForms/Shop.UIForms/ViewModels/ProductsViewModel.cs
@@ -3,37 +3,36 @@ namespace Shop.UIForms.ViewModels
 {
     using Shop.Common.Models;
     using Shop.Common.Services;
-    using System;
     using System.Collections.Generic;
     using System.Collections.ObjectModel;
-    using System.Text;
     using Xamarin.Forms;
 
-    public class ProductsViewModel:BaseViewModel
+    public class ProductsViewModel : BaseViewModel
     {
         public ObservableCollection<Product> products;
         private readonly ApiService apiService;
-        public ObservableCollection<Product> Products
-        {
-            get { return this.products; }
-            set { this.SetValue(ref this.products, value); }
-        }
+        private bool isRefreshing;
+        public ObservableCollection<Product> Products { get => products; set => SetValue(ref products, value); }
+
+        public bool IsRefreshing { get => isRefreshing; set => SetValue(ref isRefreshing, value); }
         public ProductsViewModel()
         {
-            this.apiService = new ApiService();
-            this.LoadProducts();
+            apiService = new ApiService();
+            LoadProducts();
         }
 
         private async void LoadProducts()
         {
-           var response =await this.apiService.GetListAsync<Product>("http://azul.inteligenciatotal.com","/api","/Products");
+            this.isRefreshing = true;
+            var response = await apiService.GetListAsync<Product>("http://azul.inteligenciatotal.com", "/api", "/Products");
             if (!response.IsSuccess)
             {
                 await Application.Current.MainPage.DisplayAlert("Error", response.Message, "Accept");
                 return;
             }
             var myProducts = (List<Product>)response.Result;
-            this.Products = new ObservableCollection<Product>();
+            Products = new ObservableCollection<Product>();
+            this.isRefreshing = false;
         }
     }
 }
