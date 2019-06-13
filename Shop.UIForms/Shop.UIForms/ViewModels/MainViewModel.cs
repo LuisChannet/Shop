c462bbeb519847f28530e984baf1c894a11d79b9
Channet <channet@inteligenciatotal.com>
2019-06-13 00:01:01 -0500

    LoginViewModel

--- Shop.UIForms/Shop.UIForms/ViewModels/MainViewModel.cs
+++ Shop.UIForms/Shop.UIForms/ViewModels/MainViewModel.cs
@@ -6,6 +6,11 @@ using System.Collections.Generic;
 using System.Text;
    public  class MainViewModel
     {
+        public LoginViewModel Login { get; set; }
 
+        public MainViewModel()
+        {
+            this.Login = new LoginViewModel();
+        }
     }
 }
