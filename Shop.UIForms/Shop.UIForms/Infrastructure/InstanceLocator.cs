7d7e55d0947a0d366cb8db27af5b1eb3caf466f6
Channet <channet@inteligenciatotal.com>
2019-06-12 23:49:29 -0500

    Locator

---
+++ Shop.UIForms/Shop.UIForms/Infrastructure/InstanceLocator.cs
@@ -0,0 +1,17 @@
+﻿
+namespace Shop.UIForms.Infrastructure
+{
+    using Shop.UIForms.ViewModels;
+
+    internal class InstanceLocator
+    {
+        public MainViewModel Main { get; set; }
+
+        public InstanceLocator()
+        {
+            Main = new MainViewModel();
+        }
+
+
+    }
+}
